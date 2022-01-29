c69269d462c3530cb1fb5b3cf3bd76eea0a19c6a
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-01-29 17:59:26 +0400

    fix: fix the animation & start building modal

--- scripts/services.js
+++ scripts/services.js
@@ -1,1 +1,12 @@
 "use strict";
+// Article Variables
+const gym = document.querySelector(".article-gym");
+const groupExercises = document.querySelector(".article-group-exercises");
+const swimmingPool = document.querySelector(".article-swimming-pool");
+const personalTrainers = document.querySelector(".article-personal-trainers");
+const spa = document.querySelector(".article-spa");
+const forKids = document.querySelector(".article-for-kids");
+// Modal Variables
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
