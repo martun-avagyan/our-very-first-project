170cf200477ba4cbcbbe10a9aa39cb384e2fd035
Martun <65818128+martun-avagyan@users.noreply.github.com>
2022-01-29 22:20:17 +0400

    feat: add modal for GYM

--- scripts/services.js
+++ scripts/services.js
@@ -9,4 +9,24 @@ const forKids = document.querySelector(".article-for-kids");
 // Modal Variables
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+console.dir(overlay);
 const btnCloseModal = document.querySelector(".close-modal");
+// Functions
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+gym.addEventListener("click", openModal);
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
