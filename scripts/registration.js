6e7c4caae6ce5212d86d81f934d29d1ff3b1517f
Anna Sargsyan <anna.sargsyan.818830@gmail.com>
2022-02-03 20:48:44 +0400

    feat: add logIn div in which have <Email> and <Password> inputs

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,1 +1,6 @@
-let btn = document.getElementById();
+let btn = document.getElementById("btn-login");
+// let signUpForm = document.getElementsByClassName("signup-form")[0];
+// btn.click(function () {
+//   let div = document.createElement("div");
+//   signUpForm.append(div);
+// });
