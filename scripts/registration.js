54b094e992d7e577aa4846b57a6646a84d127fae
Anna Sargsyan <anna.sargsyan.818830@gmail.com>
2022-02-04 01:53:19 +0400

    add logics in registration.js about <login> button

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,4 +1,22 @@
 let btn = document.getElementById("btn-login");
+let login = document.getElementById("login");
+let registration = document.getElementById("registration");
+
+btn.addEventListener(
+  "click",
+  (myFunction = function () {
+    if (login.classList.contains("display-none")) {
+      login.classList.remove("display-none");
+      registration.classList.add("display-none");
+    } else {
+      registration.classList.remove("display-none");
+      login.classList.add("display-none");
+    }
+  })
+);
+
+// btn.onclick();
+// btn.onclick(login.classList.add("display-none"));
 // let signUpForm = document.getElementsByClassName("signup-form")[0];
 // btn.click(function () {
 //   let div = document.createElement("div");
