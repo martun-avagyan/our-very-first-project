d503ae60546fd242a24e0e3fc9fa2e671f2b49b9
Anna Sargsyan <anna.sargsyan.818830@gmail.com>
2022-02-04 09:41:05 +0400

    fix: remove all consoles from <registration.js> file

--- scripts/registration.js
+++ scripts/registration.js
@@ -14,11 +14,3 @@ btn.addEventListener(
     }
   })
 );
-
-// btn.onclick();
-// btn.onclick(login.classList.add("display-none"));
-// let signUpForm = document.getElementsByClassName("signup-form")[0];
-// btn.click(function () {
-//   let div = document.createElement("div");
-//   signUpForm.append(div);
-// });
